472f2c62056602c9e8e5a0be1a1ddc85d577e53e
viman39 <41690510+viman39@users.noreply.github.com>
2019-06-06 14:45:44 +0300

    Delete config.php

--- database/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-//Nu modificati acest fisier fara sa anuntati!
-
-session_start();
-
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'juicy');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
