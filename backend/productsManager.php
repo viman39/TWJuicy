1db149317905bcd07ae554e461ff3b99349e354f
viman39 <41690510+viman39@users.noreply.github.com>
2019-06-08 17:22:46 +0200

    update productsManager

--- backend/productsManager.php
+++ backend/productsManager.php
@@ -7,4 +7,27 @@
  */
 
 include_once('database\Database.php');
-include_once ('..\frontend\menu.php');
+include_once('..\frontend\menu.php');
+include_once('..\frontend\builders\productManagerBuilder.php');
+
+$productsManager = new productManagerBuilder();
+
+$reader = new Reader();
+
+$email = $_SESSION['username'];
+$id_vanzator = $reader->getSellerId($email);
+$lista_produse = $reader->getProductsFrom($id_vanzator);
+
+while($row = $lista_produse->fetch_assoc()){
+    $nume = $row['nume'];
+    $pret = $row['pret'];
+    $acidulat = $row['acidulat'];
+    $arome = $row['arome'];
+    $path_poza = "\"" . "../backend/products/" . $row['path_poza'] . "\"";
+    $id_produs = $row['id_produs'];
+    $quantity = $reader->getOwnedQuantity($id_produs);
+
+    $productsManager->buildItem($nume, $pret, $acidulat, $arome, $quantity);
+}
+
+$productsManager->buildTail();
